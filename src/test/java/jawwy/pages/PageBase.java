293d7d0ab3fa48d43a48a9cbda13d5f4c4657e2a
Mohammed Hendy <92641276+MohammedHendy94@users.noreply.github.com>
2023-03-29 12:52:48 +0200

    intial commit

--- src/test/java/jawwy/pages/PageBase.java
+++ src/test/java/jawwy/pages/PageBase.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import javax.swing.*;
 import java.time.Duration;
 
 
@@ -17,6 +16,7 @@ public class PageBase {
     static WebDriver webDriver = DriverSingleton.getWebDriver();
     public Actions actions;
     public WebDriverWait wait;
+
     public PageBase(){
         actions = new Actions(webDriver);
         wait = new WebDriverWait(webDriver, Duration.ofSeconds(15));
@@ -31,10 +31,13 @@ public class PageBase {
         wait.until(ExpectedConditions.elementToBeClickable(element));
         webDriver.findElement(element).click();
     }
+
     public void navigateToUrl(String url){
         webDriver.navigate().to(url);
     }
+
     public WebElement getWebElement(By element){return webDriver.findElement(element);}
+
     public void clickElementByActions(By element){
         WebElement wElement =getWebElement(element);
         actions.moveToElement(wElement).click().build().perform();
